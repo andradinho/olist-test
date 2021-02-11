caee84b9864236d5e53bb2d686e419ac0a8d2498
Andradinho <raphaelandrade2013@gmail.com>
2021-02-11 01:08:03 -0300

    Refactoring. Setting the TestCase.php config to default.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, DatabaseMigrations;
-
-    public function setUp()
-    {
-        parent::setUp();
-        Artisan::call('db:seed');
-    }
+    use CreatesApplication;
 }
