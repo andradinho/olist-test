bcd2abfef800430dd23ae0cbf90ce1f7cde17caf
Andradinho <raphaelandrade2013@gmail.com>
2021-02-11 05:35:31 -0300

    Test to 'Create a new product'.

--- tests/Feature/Controllers/ProductControllerTest.php
+++ tests/Feature/Controllers/ProductControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductControllerTest extends TestCase
 {
@@ -31,5 +32,22 @@ class ProductControllerTest extends TestCase
         $response = $this->get("/api/products/{$product->id}");
        
         $response->assertStatus(200);
+    }
+
+    public function testCreateNewProduct()
+    {
+        $category = Category::factory()->create();
+
+        $newProduct = [
+            'name' => 'New Product',
+            'category_id' => $category->id,
+            'price' => 10.90,
+        ];
+
+        $response = $this->post('/api/products', $newProduct);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('products', $newProduct);
     }
 }
